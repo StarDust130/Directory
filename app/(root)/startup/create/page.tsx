51131eced190f84f8730ea75f3bef50c3a697f32
ChandraShekhar <csyadav0513@gmail.com>
2024-11-17 17:35:23 +0530

    Enhance startup creation page layout; add heading and adjust minimum height for better visibility

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -2,7 +2,8 @@
 const Page = () => {
   return (
     <>
-    <section className="pink_container">
+    <section className="pink_container !min-h-[230px]">
+      <h1 className="heading">Submit Your Startup Pitch</h1>
 
     </section>
     </>
